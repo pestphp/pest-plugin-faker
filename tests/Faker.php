e0baa7854bb2a01a2766315520f64647322ecdf0
Nuno Maduro <enunomaduro@gmail.com>
2025-08-20 10:04:17 +0100

    chore: prepares for pest 4.x

--- tests/Faker.php
+++ tests/Faker.php
@@ -6,7 +6,7 @@ it('has faker')
     ->assertTrue(function_exists('Pest\Faker\fake'));
 
 it('can use faker instance')
-    ->assertIsString(fake()->name);
+    ->assertIsString(fake()->name());
 
 it('can use faker with locale instance')
-    ->assertIsString(fake('fr_FR')->name);
+    ->assertIsString(fake('fr_FR')->name());
